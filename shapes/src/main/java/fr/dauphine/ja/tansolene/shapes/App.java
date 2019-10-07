56aac654527d902353f5d0fa378bc38848e3a2c0
tanso19 <tanso19@b048-11.crio.dauphine.fr>
2019-10-07 14:11:02 +0200

    shapes

---
+++ shapes/src/main/java/fr/dauphine/ja/tansolene/shapes/App.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.tansolene.shapes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
